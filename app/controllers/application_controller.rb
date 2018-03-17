ce5636571b04591cfb085b5eedd3d981319abef1
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-17 17:51:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,36 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  get '/recipes' do
+    #binding.pry
+    @recipes = Recipe.all
+    erb :recipes
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.all.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do  #load edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  delete '/recipes/:id/delete' do #delete action
+    @recipe = Recipes.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
+
+  post '/recipes' do  #creates a recipe
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
 end
